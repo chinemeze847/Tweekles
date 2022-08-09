c176403f90b99d1c8ca0f5d840174543986cf5b2
davidadediji <davidadediji@gmail.com>
2022-08-09 11:50:57 +0100

    implement service and update application.yaml

--- src/main/java/com/africa/teambox/tweekles/service/PostServiceImpl.java
+++ src/main/java/com/africa/teambox/tweekles/service/PostServiceImpl.java
@@ -1,13 +1,22 @@
 package com.africa.teambox.tweekles.service;
 
 import com.africa.teambox.tweekles.dto.PostRequestDto;
+import com.africa.teambox.tweekles.model.Post;
+import com.africa.teambox.tweekles.repository.PostRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+
+@RequiredArgsConstructor
 @Service
 public class PostServiceImpl implements PostService {
-
+   private final PostRepository postRepository;
     @Override
     public void createPost(PostRequestDto request) {
+        Post post = new Post();
+        post.setUsername(request.getUserName());
+        post.setMessage(request.getMessage());
+        postRepository.save(post);
 
     }
 }
