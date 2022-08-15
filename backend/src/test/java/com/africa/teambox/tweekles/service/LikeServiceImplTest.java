252fa32244819ebc9d1354cba7747acb4290544a
Chinemeze ThankGod <thankgodchinemeze@gmail.com>
2022-08-15 10:57:56 +0100

    added like service test class

--- backend/src/test/java/com/africa/teambox/tweekles/service/LikeServiceImplTest.java
+++ backend/src/test/java/com/africa/teambox/tweekles/service/LikeServiceImplTest.java
@@ -1,4 +1,35 @@
+package com.africa.teambox.tweekles.service;
+
+import com.africa.teambox.tweekles.repository.LikeRepository;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
 import static org.junit.jupiter.api.Assertions.*;
-class LikeServiceImplTest {
-  
+
+class LikeServiceTest
+{
+
+    LikeService service;
+
+    LikeRepository repository;
+
+    @Test
+    void likePost()
+    {
+
+    }
+
+    @Test
+    @Disabled
+    void unlikePost() {
+    }
+
+    @Test
+    @Disabled
+    void getPostLikesCount()
+    {
+    }
 }
