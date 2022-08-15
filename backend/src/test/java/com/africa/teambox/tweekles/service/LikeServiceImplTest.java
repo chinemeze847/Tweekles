525d7c225daad8e1620970b82093a473f35e246d
marienoir <okosunmaryeghonghon@gmail.com>
2022-08-15 12:15:28 +0100

    Implemented mock test for shouldLikePostIfPostIdIsPresent

--- backend/src/test/java/com/africa/teambox/tweekles/service/LikeServiceImplTest.java
+++ backend/src/test/java/com/africa/teambox/tweekles/service/LikeServiceImplTest.java
@@ -1,25 +1,65 @@
 package com.africa.teambox.tweekles.service;
 
+import com.africa.teambox.tweekles.dto.LikeRequestDto;
+import com.africa.teambox.tweekles.model.Like;
+import com.africa.teambox.tweekles.model.Post;
 import com.africa.teambox.tweekles.repository.LikeRepository;
+import com.africa.teambox.tweekles.repository.PostRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.Optional;
+import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class LikeServiceTest
 {
-
-    LikeService service;
-
+    @InjectMocks
+    LikeServiceImpl service;
+    @Mock
     LikeRepository repository;
 
-    @Test
-    void likePost()
-    {
+    @Mock
+    PostRepository postRepository;
 
+    @Mock
+    LikeRequestDto likeRequestDto;
+    private UUID postId;
+    Post post;
+    Like like;
+    @BeforeEach
+    void setUp() {
+        postId = UUID.fromString("16e3e6fa-10ab-4e30-9ed9-bfc2add3aa7f");
+
+        likeRequestDto = new LikeRequestDto();
+        likeRequestDto.setUsername("Eze");
+
+        post = new Post();
+        post.setUsername("Modupe");
+        post.setMessage("A senior backend dev");
+        post.setId(postId);
+
+        like = new Like();
+        like.setUsername("Eze");
+        like.setPost(post);
+    }
+
+    @Test
+    void shouldLikePostIfPostIdIsPresent()
+    {
+        when(repository.save(like)).thenReturn(like);
+        when(postRepository.findById(postId)).thenReturn(Optional.of(post));
+        Like actualLike = service.likePost(String.valueOf(postId), likeRequestDto);
+
+        assertThat(actualLike).isEqualTo(like);
     }
 
     @Test
