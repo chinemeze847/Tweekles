3a1288c8f98c74273b16e60e3f497dd8242b50e3
davidadediji <davidadediji@gmail.com>
2022-08-18 05:19:49 +0100

    fix conflicts

--- frontend/tweekles-frontend/src/App.tsx
+++ frontend/tweekles-frontend/src/App.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import './App.css';
-<<<<<<< HEAD
 import { Card } from './components/Card';
 import Post from './pages/post/Post';
-
-
-=======
 import { Login } from './pages/login/Login';
->>>>>>> 3946427a8dbe9ce108ae5b0c782378477d7a908d
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <Post/>
-=======
       <Login/>
->>>>>>> 3946427a8dbe9ce108ae5b0c782378477d7a908d
     </div>
   );
 }
